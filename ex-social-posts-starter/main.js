a60dad52808ef1b22d74af988297ef966923a2d9
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-03 16:50:56 +0200

    milestone 1

---
+++ ex-social-posts-starter/main.js
@@ -0,0 +1,73 @@
+// **Milestone 1** - Creiamo il nostro array di oggetti che rappresentano ciascun post (come da esempio).
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// - id del post, numero progressivo da 1 a n
+// - nome autore,
+// - foto autore,
+// - data in formato americano (mm-gg-yyyy: es 05-03-2022),
+// - testo del post,
+// - immagine (non tutti i post devono avere una immagine),
+// - numero di likes.
+// *Non è necessario creare date casuali*
+// *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)*
+// **Milestone 2** - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+// **Milestone 3** - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+
+
+const posts = [{
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Larry Bird",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 800,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Pat Riley",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 867,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Steve Kerr",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 300,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Greg Popovich",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 500,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Jackson",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 800,
+        "created": "2021-06-25"
+    },
+];
